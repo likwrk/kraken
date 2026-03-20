43f37de16e2f54d88991913ac86c28113e213e1b
Pavel B <likwrk@gmail.com>
2026-03-20 12:52:40 +0300

    provide default base url

--- app/js/config.js
+++ app/js/config.js
@@ -3,6 +3,7 @@
  */
 export const Config = {
   STORAGE_KEY: "APP_API_BASE_URL",
+  DEFAULT_BASE_URL: "http://localhost:12345",
 
   getUrl() {
     return sessionStorage.getItem(this.STORAGE_KEY);
@@ -22,6 +23,7 @@ export const Config = {
     if (!url) {
       url = prompt(
         "Please enter the Base API URL (e.g., https://api.example.com):",
+        this.DEFAULT_BASE_URL,
       );
       if (url) {
         this.setUrl(url);
